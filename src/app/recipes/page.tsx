df1ac5c4d045f868caf63dbecad6be34e17ab479
leechungyoung <LeeChungYoung@woore.co.kr>
2025-09-14 13:49:03 +0900

    fix: Explicitly type uniqueWeeks for sorting

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -30,7 +30,7 @@ export default function AllRecipesPage() {
         if (data.recipes) {
           setRecipes(data.recipes);
           setFilteredRecipes(data.recipes);
-          const uniqueWeeks = [...new Set(data.recipes.map((r: Recipe) => r.week))].sort((a: number, b: number) => a - b);
+          const uniqueWeeks: number[] = [...new Set(data.recipes.map((r: Recipe) => r.week))].sort((a: number, b: number) => a - b);
           setWeeks(uniqueWeeks);
         }
       })
